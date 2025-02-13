5415e9ee9106969137163cfd43d3d3828fe2926e
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2025-02-13 12:15:47 +0700

    refactor: update cross-env dependency and extract RandomUserForm component

---
+++ src/pages/RandomUser/RandomUserForm.tsx
@@ -0,0 +1,50 @@
+import { Button, Form, Input } from 'antd';
+import { FC } from 'react';
+
+interface RandomUserFormProps {
+	row?: RandomUser.Record;
+	isEdit: boolean;
+	setVisible: (visible: boolean) => void;
+	getDataUser: () => void;
+}
+
+const RandomUserForm: FC<RandomUserFormProps> = ({ row, isEdit, setVisible, getDataUser }) => {
+	const [form] = Form.useForm();
+
+	const onFinish = (values: any) => {
+		if (isEdit) {
+			const dataLocal: any = JSON.parse(localStorage.getItem('data') as any);
+			const newData = dataLocal.map((item: any) => {
+				if (item.address === row?.address) {
+					return values;
+				}
+				return item;
+			});
+			localStorage.setItem('data', JSON.stringify(newData));
+		} else {
+			const dataLocal: any = JSON.parse(localStorage.getItem('data') as any) || [];
+			dataLocal.push(values);
+			localStorage.setItem('data', JSON.stringify(dataLocal));
+		}
+		getDataUser();
+		setVisible(false);
+	};
+
+	return (
+		<Form form={form} initialValues={row} onFinish={onFinish} layout='vertical'>
+			<Form.Item label='Address' name='address' rules={[{ required: true, message: 'Please input address!' }]}>
+				<Input />
+			</Form.Item>
+			<Form.Item label='Balance' name='balance' rules={[{ required: true, message: 'Please input balance!' }]}>
+				<Input />
+			</Form.Item>
+			<Form.Item>
+				<Button type='primary' htmlType='submit'>
+					{isEdit ? 'Update' : 'Submit'}
+				</Button>
+			</Form.Item>
+		</Form>
+	);
+};
+
+export default RandomUserForm;
--- src/pages/RandomUser/index.tsx
+++ src/pages/RandomUser/index.tsx
@@ -1,7 +1,8 @@
 import type { IColumn } from '@/components/Table/typing';
-import { Button, Form, Input, Modal, Table } from 'antd';
+import { Button, Modal, Table } from 'antd';
 import { useEffect, useState } from 'react';
 import { useModel } from 'umi';
+import RandomUserForm from './RandomUserForm';
 
 const RandomUser = () => {
 	const { data, getDataUser } = useModel('randomuser');
@@ -82,40 +83,7 @@ const RandomUser = () => {
 					setVisible(false);
 				}}
 			>
-				<Form
-					onFinish={(values) => {
-						const index = data.findIndex((item: any) => item.address === row?.address);
-						const dataTemp: RandomUser.Record[] = [...data];
-						dataTemp.splice(index, 1, values);
-						const dataLocal = isEdit ? dataTemp : [values, ...data];
-						localStorage.setItem('data', JSON.stringify(dataLocal));
-						setVisible(false);
-						getDataUser();
-					}}
-				>
-					<Form.Item
-						initialValue={row?.address}
-						label='address'
-						name='address'
-						rules={[{ required: true, message: 'Please input your address!' }]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						initialValue={row?.balance}
-						label='balance'
-						name='balance'
-						rules={[{ required: true, message: 'Please input your balance!' }]}
-					>
-						<Input />
-					</Form.Item>
-					<div className='form-footer'>
-						<Button htmlType='submit' type='primary'>
-							{isEdit ? 'Save' : 'Insert'}
-						</Button>
-						<Button onClick={() => setVisible(false)}>Cancel</Button>
-					</div>
-				</Form>
+				<RandomUserForm row={row} isEdit={isEdit} setVisible={setVisible} getDataUser={getDataUser} />
 			</Modal>
 		</div>
 	);
